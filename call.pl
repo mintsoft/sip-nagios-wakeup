02a73718c23546b7a1e5af0f174fe8a28bf108a2
Rob <rob@mintsoft.net>
2016-08-03 21:41:36 +0100

    Removing most of the waffle; calling syntax is now --to=01234

--- call.pl
+++ call.pl
@@ -6,46 +6,31 @@ use warnings;
 use Getopt::Long qw(:config posix_default bundling);
 use Net::SIP;
 
-sub usage {
-    print STDERR "ERROR: @_\n" if @_;
-    print STDERR <<END;
-usage: $0 [ options ]  telephone number
-Options:
-  -R|--registrar host[:port]   SIP registrar
-  -S|--send filename           audio file to play
-  --username name
-  --password pass
-END
-    exit( @_ ? 1:0 );
-}
-
-
-my ($file,$registrar,$username,$password);
+my ($to, $registrar, $username, $password);
 
 GetOptions(
-    'R|registrar=s' => \$registrar,
-#    'S|send=s' => \$file,
-    'username=s' =>\$username,
-    'password=s' =>\$password,
+    'r|registrar=s' => \$registrar,
+    'u|username=s' =>\$username,
+    'p|password=s' =>\$password,
+    't|to=s' => \$to,
 );
-my( $to ) = shift;
-$to || usage( "You must specify a phone number!" );
 
 my $ua = Net::SIP::Simple->new(
     registrar => $registrar,
     domain => $registrar,
     from => $username,
-    auth => [ $username,$password ],
+    auth => [$username, $password]
 );
 
-$ua->register( expires => 60 ) || die ( "Registration failed" . $ua->error );
+$ua->register(expires => 10) || die "Registration failed: $ua->error";
 
 my $rtp_done = 1;
 my $call = $ua->invite(
     $to,
     asymetric_rtp => 0,
     rtp_param => [ 8, 160, 160/8000, 'PCMA/8000' ],
-) || die "Invitation failed: " . $ua->error;
+);
+die "Invitation failed: $ua->error" unless $call;
 
 $ua->loop( 5, \$rtp_done );
 $call->bye;
