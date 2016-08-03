ecac874636dfc9d18ed34a48c6981ea11c309b66
Rob <rob@mintsoft.net>
2016-08-03 21:37:21 +0100

    Calls are now taken and immediately hangs up

--- call.pl
+++ call.pl
@@ -5,19 +5,17 @@ use warnings;
 
 use Getopt::Long qw(:config posix_default bundling);
 use Net::SIP;
-#Usage
 
 sub usage {
     print STDERR "ERROR: @_\n" if @_;
-    print STDERR <<EOS;
-usage: $0 [ options ]  no de tel 
-Joue un fichier vers un téléphone après que le correspondant décroche
+    print STDERR <<END;
+usage: $0 [ options ]  telephone number
 Options:
-  -R|--registrar host[:port]   register at given address
-  -S|--send filename           Fichier audio
-  --username name              
-  --password pass              probablement mieux à faire pour le cacher d'un ps
-EOS
+  -R|--registrar host[:port]   SIP registrar
+  -S|--send filename           audio file to play
+  --username name
+  --password pass
+END
     exit( @_ ? 1:0 );
 }
 
@@ -26,44 +24,28 @@ my ($file,$registrar,$username,$password);
 
 GetOptions(
     'R|registrar=s' => \$registrar,
-    'S|send=s' => \$file,
+#    'S|send=s' => \$file,
     'username=s' =>\$username,
     'password=s' =>\$password,
-) || usage( "bad option" );
-my( $to )=@ARGV;
-$to || usage( "pas de destination" );
+);
+my( $to ) = shift;
+$to || usage( "You must specify a phone number!" );
 
-# create new agent
-
-
-print "Creating connection\n";
 my $ua = Net::SIP::Simple->new(
-registrar => $registrar,
-domain => $registrar,
-from => $username,
-auth => [ $username,$password ],
-
+    registrar => $registrar,
+    domain => $registrar,
+    from => $username,
+    auth => [ $username,$password ],
 );
 
-# Register agent
+$ua->register( expires => 60 ) || die ( "Registration failed" . $ua->error );
 
-$ua->register( expires => 1800 ) # <- Valeur mini chez free
-
-|| die ( "Pas enregistré " . $ua->error );
-print "Enregistré\n";
-# Variables d'arret.(sort de loop quand rtp_done est vrai)
-
-my $rtp_done;
-print "Appelle ".$to.'@'.$registrar."\n";
-my $call= $ua->invite( $to,
-    init_media => $ua->rtp( 'send_recv', $file ),
-    cb_rtp_done => \$rtp_done,
+my $rtp_done = 1;
+my $call = $ua->invite(
+    $to,
     asymetric_rtp => 0,
     rtp_param => [ 8, 160, 160/8000, 'PCMA/8000' ],
+) || die "Invitation failed: " . $ua->error;
 
-) ||die "invite failed: ".$ua->error;
-
-# Mainloop
-
-$ua->loop( \$rtp_done );
+$ua->loop( 5, \$rtp_done );
 $call->bye;
