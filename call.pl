d763db679ce86f66ac33f6c99c17efcdb93d3f48
Rob <rob@mintsoft.net>
2016-08-04 21:32:20 +0100

    Enforcing a 60 second timeout on the invite; when this fails it does not terminate the current call

--- call.pl
+++ call.pl
@@ -25,16 +25,22 @@ my $ua = Net::SIP::Simple->new(
 
 $ua->register(expires => 10) || die "Registration failed: " . dump($ua->error);
 
-my $call = $ua->invite(
-    $to,
-    asymetric_rtp => 0,
-    rtp_param => [8, 160, 160/8000, 'PCMA/8000'],
-);
-die "Invitation failed: " . dump($ua->error) unless $call;
+my $failedCall = 1;
+eval {
+    local $SIG{ALRM} = sub { die "timed-out\n" };
+    alarm 60;
 
-my $failedCall = defined $call->{'last_error'};
+    my $call = $ua->invite(
+        $to,
+        asymetric_rtp => 0,
+        rtp_param => [8, 160, 160/8000, 'PCMA/8000'],
+    );
+    die "Invitation failed: " . dump($ua->error) unless $call;
+    alarm 0;
 
-$call->bye;
-
+    $failedCall = 0 if not defined $call->{'last_error'};
+    $call->bye;    
+};
+$ua->cleanup();
 exit 1 if $failedCall;
 exit 0;
