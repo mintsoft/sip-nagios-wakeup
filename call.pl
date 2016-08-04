eecdcbb781cce537934bb907efaff8c2ffa1a044
Rob <rob@mintsoft.net>
2016-08-04 22:15:44 +0100

    Removing timeout and using ring_time (more SIP friendly); however I now can't tell between ringing out and success

--- call.pl
+++ call.pl
@@ -20,27 +20,25 @@ my $ua = Net::SIP::Simple->new(
     registrar => $registrar,
     domain => $registrar,
     from => $username,
-    auth => [$username, $password]
+    auth => [$username, $password],
+cb_noanswer => sub{  die "BROKEN"; }
 );
 
 $ua->register(expires => 10) || die "Registration failed: " . dump($ua->error);
 
 my $failedCall = 1;
-eval {
-    local $SIG{ALRM} = sub { die "timed-out\n" };
-    alarm 60;
+my $call = $ua->invite(
+    $to,
+    asymetric_rtp => 0,
+    rtp_param => [8, 160, 160/8000, 'PCMA/8000'],
+    ring_time => 10,
+);
 
-    my $call = $ua->invite(
-        $to,
-        asymetric_rtp => 0,
-        rtp_param => [8, 160, 160/8000, 'PCMA/8000'],
-    );
-    die "Invitation failed: " . dump($ua->error) unless $call;
-    alarm 0;
+die "Invitation failed: " . dump($ua->error) unless $call;
+$failedCall = 0 if not defined $call->{'last_error'};
 
-    $failedCall = 0 if not defined $call->{'last_error'};
-    $call->bye;    
-};
+$call->bye;    
 $ua->cleanup();
+
 exit 1 if $failedCall;
 exit 0;
